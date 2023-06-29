c6a88dc5e287c2b5d70c5299496a872fb3fcfeb8
Maicon Talgatti <dionestalgattimaicon@gmail.com>
2023-06-29 14:04:51 -0300

    Responsividade check

--- resources/views/records/form_people.blade.php
+++ resources/views/records/form_people.blade.php
@@ -5,34 +5,9 @@
         </h2>
     </x-slot>
 
-    <!-- Bloco totalmente responsivo-->
-    <div style="padding-top: 10px!important;padding-bottom: 10px!important;">
+
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-6">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                        <div class="sm:flex">
-                            <div style="width: 100%;" class="sm:w-40">
-                                <label for="city1" class="dark:text-gray-100 block text-sm font-medium leading-6 text-gray-900 new_cs">City 1</label>
-                                <div class="mt-12">
-                                    <input type="text" name="city1" id="city1" autocomplete="address-level2" class="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </div>
-                            </div>
-                            <div style="width: 10%;">
-                            </div>
-                            <div style="width: 100%;" class="sm:w-40">
-                                <label for="city2" class="dark:text-gray-100 block text-sm font-medium leading-6 text-gray-900">City 2</label>
-                                <div class="mt-12">
-                                    <input type="text" name="city2" id="city2" autocomplete="address-level2" class="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
- 
     <section class="py-3">
         <div class="container px-4 mx-auto">
             <div class="p-8 dark:bg-gray-800 bg-white rounded-xl">
@@ -132,37 +107,10 @@
                     </div>
                 </form>
             </div>
+         </div>
+            </section>
         </div>
-    </section>
-
-
-    <div style="padding-top: 10px!important;padding-bottom: 10px!important;">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" >
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-
-                        <!-- <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Badge</span>-->
-                        <div class="mt-6" style="margin-top: 0px!important">
-                            <a href="#" class="flex items-center justify-center rounded-md border border-transparent px-6 py-3 text-base font-medium text-white shadow-sm">
-                                <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                        style="padding: 10px 50px;">
-                                    Cadastrar nova pessoa
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="margin-top: 10px">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Menu de cadastros de pessoas!") }}
-                </div>
-            </div>
-        </div>
-
-
-
     </div>
+
+
 </x-app-layout>
