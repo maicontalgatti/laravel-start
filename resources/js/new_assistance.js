dc887ead211e6252da67c287c0bac07227ea89ad
Maicon Talgatti <dionestalgattimaicon@gmail.com>
2023-09-12 13:31:57 -0300

    éxibe projetos, metade do responsivo

--- resources/js/new_assistance.js
+++ resources/js/new_assistance.js
@@ -21,4 +21,3 @@ if (document.querySelector('#garantia')) {
 
 };
 
-
--- resources/views/assistance/index.blade.php
+++ resources/views/assistance/index.blade.php
@@ -23,38 +23,56 @@
             </div>
         </div>
 
-
         @foreach($assistencias as $assistencia)
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="margin-top: 5px">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <div class="flex ">
-                            <div class="w-2/6">{{$assistencia->descricao}}</div>
-                                <?php
-                                if($assistencia->status == "Aberto"){
-                                        echo "<div style='color:red' class='w-1/6'>".$assistencia->status."</div>";
-                                    }else{
-                                        echo "<div style='color:green' class='w-1/6'>".$assistencia->status."</div>";
-                                    };
-                                ?>
-                                <div class="w-1/6">{{$assistencia->tipo_assistencia}}</div>
-                                <div class="w-1/6">{{$assistencia->cliente->nome}}</div>
-                                <div class="w-1/6">
-                                        <?php echo date("d-m-Y", strtotime($assistencia->data_chamado)); ?>
-                                </div>
-
-                                <div class="w-1/6 ">
-                                    <a href=" "  class="flex items-center justify-center rounded-md border border-transparent text-base font-medium text-white shadow-sm ">
-                                        <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                            Editar
-                                        </button>
-                                    </a>
-                                </div>
+                        <div class="row">
+                            <!-- Coluna 1: Descrição da Assistência -->
+                            <div class="col-md-4 col-12">
+                                {{$assistencia->descricao}}
                             </div>
+
+                            <!-- Coluna 2: Status -->
+                            <div class="col-md-4 col-12">
+                                {{$assistencia->status}}
+                            </div>
+
+                            <!-- Coluna 3: Botão de Mostrar Informações (Visível apenas em dispositivos móveis) -->
+                            <div class="col-md-4 col-12 text-center d-md-none">
+                        <span class="show-more" onclick="toggleExtraInfo(this)">
+                            Mostrar Mais <i class="fas fa-chevron-down"></i>
+                        </span>
+                            </div>
+                        </div>
+
+                        <!-- Mostrar informações adicionais quando clicado (Visível apenas em dispositivos móveis) -->
+                        <div class="extra-info d-md-none" style="display: none;">
+                            <!-- Coloque aqui as informações adicionais -->
+                            {{$assistencia->tipo_assistencia}}
+                            {{$assistencia->cliente->nome}}
+                                <?php echo date("d-m-Y", strtotime($assistencia->data_chamado)); ?>
+                            <a href="#" class="edit-button">Editar</a>
+                        </div>
                     </div>
                 </div>
             </div>
-
         @endforeach
+
+        <!-- Adicione a biblioteca Font Awesome para o ícone de seta -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+        <script>
+            function toggleExtraInfo(showMoreElement) {
+                var extraInfo = showMoreElement.parentElement.parentElement.parentElement.querySelector('.extra-info');
+                if (extraInfo.style.display === 'none' || extraInfo.style.display === '') {
+                    extraInfo.style.display = 'block';
+                } else {
+                    extraInfo.style.display = 'none';
+                }
+            }
+        </script>
+
+
     </div>
 </x-app-layout>
